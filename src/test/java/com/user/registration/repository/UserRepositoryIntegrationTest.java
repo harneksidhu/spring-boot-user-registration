9d4a8e24361d12457e4250f6f4ce0b38f2d0c74e
Harnek Sidhu <h6sidhu@gmail.com>
2016-10-31 21:10:53 -0400

    Repository integration testing

---
+++ src/test/java/com/user/registration/repository/UserRepositoryIntegrationTest.java
@@ -0,0 +1,66 @@
+package com.user.registration.repository;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.junit.Assert.assertThat;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.user.registration.SpringBootUserRegistrationApplication;
+import com.user.registration.model.User;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes=SpringBootUserRegistrationApplication.class)
+public class UserRepositoryIntegrationTest {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	User user;
+	
+	@Before
+	public void init(){
+		user = new User();
+		user.setUserName("testUser");
+		user.setPassword("testPassword1");
+		userRepository.save(user);
+	}
+	
+	@After
+	public void cleanup(){
+		userRepository.delete(user);
+	}
+	
+	@Test
+	public void testFindOneByUserNameAndPassword(){
+		User userQuery = userRepository.findOneByUserNameAndPassword(user.getUserName(), user.getPassword());
+		assertThat(userQuery.getUserName(), is(user.getUserName()));	
+		assertThat(userQuery.getPassword(), is(user.getPassword()));
+	}
+	
+	@Test
+	public void testFindOneByUserNameAndPasswordEmpty(){
+		User userQuery = userRepository.findOneByUserNameAndPassword("user", "pass");
+		assertThat(userQuery, is(nullValue()));	
+	}
+	
+	@Test
+	public void testFindByUserName(){
+		User userQuery = userRepository.findOneByUserName(user.getUserName());
+		assertThat(userQuery.getUserName(), is(user.getUserName()));	
+		assertThat(userQuery.getPassword(), is(user.getPassword()));
+	}
+	
+	@Test
+	public void testFindByUserNameEmpty(){
+		User userQuery = userRepository.findOneByUserName("user");
+		assertThat(userQuery, is(nullValue()));	
+	}
+}
